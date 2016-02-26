b7b011415052a5e13c79f54c2275b2da4ed1cb61
bmart <bmart@DESKTOP-0NUR6ON>
2016-02-25 20:10:56 -0500

    added doGet {account.getBalance()}

--- src/java/servlet/AccountServlet.java
+++ src/java/servlet/AccountServlet.java
@@ -16,13 +16,22 @@
 
 package servlet;
 
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Account;
 
 /**
  * Provides an Account Balance and Basic Withdrawal/Deposit Operations
  */
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
-    
+  Account account = new Account();   
+  
+  @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      account.getBalance();
+    }
 }
